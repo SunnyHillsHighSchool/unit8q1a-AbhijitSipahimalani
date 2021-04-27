696dda25e461f6155b9e061ebbfad1b076f46b9d
Abhijit Sipahimalani <>
2021-04-27 23:00:36 +0000

    Finished working on code

--- Unit8_1a.java
+++ Unit8_1a.java
@@ -1,8 +1,15 @@
 public class Unit8_1a
 {
   public int [] [] modifyColumns (int [] [] arr)
- { 
-return arr;
+ {
+  for(int i = 0; i < arr.length; i++){
+    for(int j = 0; j < arr[0].length; j++){
+      if(i % 2 != 0){
+        arr [i] [j] = arr [i-1] [j];
       }
     }
+  }
+  return arr;
+  }
+}
  
